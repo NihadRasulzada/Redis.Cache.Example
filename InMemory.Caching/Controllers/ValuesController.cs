4dc7fb07079659d57ed7496a8ea570309c6e4812
NihadRasulzada <dev.nihad.rasulzada@outlook.com>
2026-01-16 19:56:54 +0400

    Add ValuesController for in-memory date caching API
    Added ValuesController with endpoints to set and get a cached date using IMemoryCache, including expiration policies. Removed unused Controllers folder reference from project file.

---
+++ InMemory.Caching/Controllers/ValuesController.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+namespace InMemory.Caching.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController(IMemoryCache memoryCache) : ControllerBase
+    {
+        //[HttpGet("set/{name}")]
+        //public void SetName(string name)
+        //{
+        //    memoryCache.Set("name", name);
+        //}
+
+        //[HttpGet("get")]
+        //public string GetName()
+        //{
+        //    return memoryCache.Get<string>("name") ?? "Value not found";
+        //}
+
+        [HttpGet("setDate")]
+        public void SetDate()
+        {
+            memoryCache.Set("date", DateTime.Now, options: new()
+            {
+                AbsoluteExpiration = DateTime.Now.AddSeconds(30),
+                SlidingExpiration = TimeSpan.FromSeconds(5)
+            });
+        }
+
+        [HttpGet("getDate")]
+        public DateTime GetDate()
+        {
+            return memoryCache.Get<DateTime>("date");
+        }
+    }
+}
